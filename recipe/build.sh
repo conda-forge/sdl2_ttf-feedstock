61e1cc0737f2bb27d471d62aabb087b23cbc2df6
Silvio Traversaro <silvio@traversaro.it>
2024-03-23 17:52:35 +0100

    Fix wrong library named used for linking in macos and add symlink for backcompatibility

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,3 +14,12 @@ cmake -G Ninja \
 
 cmake --build .
 cmake --install .
+
+# For backward compatibility with old builds of sdl2_ttf that linked with strangly-named
+# version of the libraries, we define some symlinks, remove once the library is updated to sdl3
+if [[ "${target_platform}" == osx-* ]]; then
+    ln -s ${PREFIX}/lib/libSDL2_ttf-2.0.0.dylib ${PREFIX}/lib/libSDL2_ttf-2.0.2201.0.0.dylib
+    ln -s ${PREFIX}/lib/libSDL2_ttf-2.0.0.dylib ${PREFIX}/lib/libSDL2_ttf-2.0.2001.2.0.dylib
+    ln -s ${PREFIX}/lib/libSDL2_ttf-2.0.0.dylib ${PREFIX}/lib/libSDL2_ttf-2.0.2001.1.0.dylib
+    ln -s ${PREFIX}/lib/libSDL2_ttf-2.0.0.dylib ${PREFIX}/lib/libSDL2_ttf-2.0.15.0.0.dylib
+fi
